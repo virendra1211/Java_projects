9135173f42fa347de64de696006077b7f5a69376
virendra1211 <khadevirendra@gmail.com>
2021-08-23 06:13:49 +0530

    Updated file

--- MyProject/src/org/test/JavaPractice.java
+++ MyProject/src/org/test/JavaPractice.java
@@ -33,7 +33,7 @@ System.out.println();
 			n1 = n2;
 			n2 = n3;
 		}
-		System.out.println("  even sum = "+sum);
+		System.out.println("  even sum1 = "+sum);
 	}
 	
 	public static int multiply(int A,int B) {
